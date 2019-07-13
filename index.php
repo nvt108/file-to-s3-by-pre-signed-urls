175986bb8981b22dba974b8aac2b7787fcee8310
Trung Nguyen <nvtrung108@gmail.com>
2019-07-13 22:17:13 +0700

    create files default

---
+++ index.php
@@ -0,0 +1,8 @@
+<script type="text/javascript" src="assets/main.js"></script>
+<link rel="stylesheet" type="text/css" href="assets/style.css" />
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+<div class="main">
+    <label >Choose your file:</label>
+    <input type="file" name="yourfile" />
+    <button type="submit">Upload</button>
+</div>
